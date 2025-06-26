2c179e83f83d486994602e74c79b26a9ca8c2114
umai-beer <umai-beer@hotmail.co.jp>
2025-06-26 22:35:51 +0900

    ok

---
+++ CshrpModelToDartModel/CshrpModelToDartModel/Options.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CshrpModelToDartModel
+{
+    public class Options
+    {
+        [Option('s',"source",HelpText ="変換するファイルまたはディリクトリ")]
+        public string InputPath { get; set; } = string.Empty;
+        [Option('f',"filename", HelpText = "変換後のファイル")]
+        public string OutputPath { get; set; } = string.Empty;
+        [Option("subdir", HelpText = "ソースオプションがDIRのときサブDIRまで探索するか")]
+        public bool IsSubDir { get; set; } = false;
+        [Option('r', "regex", HelpText = "抽出するファイル名の正規表現。")]
+        public string RegexPattern { get; set; } =string.Empty;
+    }
+}
